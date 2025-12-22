3cda680a9e219bc576c9c97f43780131f41a7cfb
Ayush <140370865+Ayyayush@users.noreply.github.com>
2025-12-22 11:58:43 +0530

    Updates

--- Database/database.js
+++ Database/database.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
-export default async function DBConnection() {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error("MongoDB connection failed ❌");
-        console.error(error.message);
-        process.exit(1);
-    }
-}
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
